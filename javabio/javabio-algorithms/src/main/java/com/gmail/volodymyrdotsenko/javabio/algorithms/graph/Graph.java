42b431e875c8cdb49b048d375ac02d5fbf258122
volodymyrd <volodymyrdotsenko@gmail.com>
2016-07-12 23:30:31 +0300

    graph

---
+++ javabio/javabio-algorithms/src/main/java/com/gmail/volodymyrdotsenko/javabio/algorithms/graph/Graph.java
@@ -0,0 +1,148 @@
+package com.gmail.volodymyrdotsenko.javabio.algorithms.graph;
+
+import com.gmail.volodymyrdotsenko.javabio.algorithms.collections.LinkedBag;
+import com.gmail.volodymyrdotsenko.javabio.algorithms.collections.LinkedStack;
+
+/**
+ * The <tt>Graph</tt> class represents an undirected graph of vertices
+ * named 0 through <em>V</em> - 1.
+ * It supports the following two primary operations: add an edge to the graph,
+ * iterate over all of the vertices adjacent to a vertex. It also provides
+ * methods for returning the number of vertices <em>V</em> and the number
+ * of edges <em>E</em>. Parallel edges and self-loops are permitted.
+ * <p>
+ * This implementation uses an adjacency-lists representation, which
+ * is a vertex-indexed array of {@link LinkedBag} objects.
+ * All operations take constant time (in the worst case) except
+ * iterating over the vertices adjacent to a given vertex, which takes
+ * time proportional to the number of such vertices.
+ *
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
+ */
+public class Graph {
+    private final int V;                //number of vertices
+    private int E;                      //number of edges
+    private LinkedBag<Integer>[] adj;   //adjacency lists
+
+    /**
+     * Initializes an empty graph with <tt>V</tt> vertices and 0 edges.
+     * param V the number of vertices
+     *
+     * @param V number of vertices
+     * @throws IllegalArgumentException if <tt>V</tt> < 0
+     */
+    public Graph(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = new LinkedBag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new LinkedBag<Integer>();
+        }
+    }
+
+    /**
+     * Initializes a new graph that is a deep copy of <tt>G</tt>.
+     *
+     * @param G the graph to copy
+     */
+    public Graph(Graph G) {
+        this(G.V());
+        this.E = G.E();
+        for (int v = 0; v < G.V(); v++) {
+            // reverse so that adjacency list is in same order as original
+            LinkedStack<Integer> reverse = new LinkedStack<>();
+            for (int w : G.adj[v]) {
+                reverse.push(w);
+            }
+            for (int w : reverse) {
+                adj[v].add(w);
+            }
+        }
+    }
+
+    /**
+     * Returns the number of vertices in this graph.
+     *
+     * @return the number of vertices in this graph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in this graph.
+     *
+     * @return the number of edges in this graph
+     */
+    public int E() {
+        return E;
+    }
+
+    /**
+     * Adds the undirected edge v-w to this graph.
+     *
+     * @param v one vertex in the edge
+     * @param w the other vertex in the edge
+     * @throws IndexOutOfBoundsException unless both 0 <= v < V and 0 <= w < V
+     */
+    public void addEdge(int v, int w) {
+        validateVertex(v);
+        validateVertex(w);
+        E++;
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    /**
+     * Returns the vertices adjacent to vertex <tt>v</tt>.
+     *
+     * @param v the vertex
+     * @return the vertices adjacent to vertex <tt>v</tt>, as an iterable
+     * @throws IndexOutOfBoundsException unless 0 <= v < V
+     */
+    public Iterable<Integer> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    /**
+     * Returns the degree of vertex <tt>v</tt>.
+     *
+     * @param v the vertex
+     * @return the degree of vertex <tt>v</tt>
+     * @throws IndexOutOfBoundsException unless 0 <= v < V
+     */
+    public int degree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    /**
+     * Returns a string representation of this graph.
+     *
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     * followed by the <em>V</em> adjacency lists
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V).append(" vertices, ").append(E).append(" edges ").append(NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v).append(": ");
+            for (int w : adj[v]) {
+                s.append(w).append(" ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    // throw an IndexOutOfBoundsException unless 0 <= v < V
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V - 1));
+    }
+}
